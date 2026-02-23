a79958b4b6ecdd8ea9c7c64f12cf351c422e35f5
Kmuzaki <keihanmuzaki0480@gmail.com>
2026-02-23 22:09:22 +0800

    twoButtonRow

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import { Image, StyleSheet, View } from 'react-native'
+import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 const index = () => {
   return (
     <View style={[styles.container,]}>
-        <View style={{flex: 1, backgroundColor: 'red'}} />
+        <View style={{height: '5%', backgroundColor: 'rgb(252, 222, 0)'}} />
         
         <View style={styles.center}>
             <Image style={styles.profilePic} source={require('../../assets/images/klonoa.jpg')} />
+            <Text style={styles.nameText}>Klonoa</Text>
+
+            <View style={styles.buttonRowDirection}>
+                <TouchableOpacity style={styles.buttonRow1} onPress={() => Alert.alert('You have pressed me. Wahoo!!')}>
+                    <Text>Wahoo!!</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.buttonRow1, styles.buttonRow2]} onPress={() => Alert.alert('You have pressed me. Wahoo!!')}>
+                    <Text>Manya-!</Text>
+                </TouchableOpacity>
+            </View>                 
         </View>
+        <View style={{height: '2%', backgroundColor: 'rgb(32, 233, 173))'}} />
+
+
     </View>
     // <View>
     //     <View style={styles.textContainer}>
@@ -46,6 +59,31 @@ const styles = StyleSheet.create({
         height: 200,
         borderRadius: 10,
         margin: 20
+    },
+
+    nameText: {
+        color: 'rgb(29, 95, 237)',
+        fontSize: 40,
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+
+    buttonRowDirection: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        // width: '60%'
+    },
+
+    buttonRow1: {
+        // width: 200,
+        // height: 100,
+        paddingVertical: 20,
+        paddingHorizontal: '75%',
+        backgroundColor: 'rgb(118, 162, 255)',
+    },
+
+    buttonRow2: {
+        backgroundColor: 'rgb(237, 154, 136)'
     }
 })
 
