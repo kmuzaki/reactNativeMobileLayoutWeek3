f5eee565b29fd1b5976412ec70256ed332222486
Kmuzaki <keihanmuzaki0480@gmail.com>
2026-02-24 09:01:20 +0800

    imageGridAndBottomButton

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,49 @@
-import React from 'react'
-import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react';
+import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const GRID_IMAGES = [
+  require('../../assets/images/ferrisWheel.png'),
+  require('../../assets/images/goKarts.png'),
+  require('../../assets/images/maze.png'),
+  require('../../assets/images/merryGoRound.png'),
+  require('../../assets/images/RCLoop.png'),
+  require('../../assets/images/steamTrains.png'),
+];
 
 const index = () => {
   return (
     <View style={[styles.container,]}>
-        <View style={{height: '5%', backgroundColor: 'rgb(252, 222, 0)'}} />
+        <View style={{height: '7%', backgroundColor: 'rgb(252, 222, 0)'}} />
         
         <View style={styles.center}>
             <Image style={styles.profilePic} source={require('../../assets/images/klonoa.jpg')} />
             <Text style={styles.nameText}>Klonoa</Text>
 
             <View style={styles.buttonRowDirection}>
-                <TouchableOpacity style={styles.buttonRow1} onPress={() => Alert.alert('You have pressed me. Wahoo!!')}>
+                <TouchableOpacity style={styles.buttonRow1} onPress={() => Alert.alert('You have pressed the button. Wahoo!!')}>
                     <Text>Wahoo!!</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={[styles.buttonRow1, styles.buttonRow2]} onPress={() => Alert.alert('You have pressed me. Wahoo!!')}>
-                    <Text>Manya-!</Text>
+                <TouchableOpacity style={[styles.buttonRow1, styles.buttonRow2]} onPress={() => Alert.alert('Meep')}>
+                    <Text>Meep</Text>
                 </TouchableOpacity>
             </View>                 
         </View>
-        <View style={{height: '2%', backgroundColor: 'rgb(32, 233, 173))'}} />
+        <View style={{height: '2%', backgroundColor: 'rgb(141, 255, 221))'}} />
 
+        {/* Code credit to mah friend */}
+        <View style={styles.gridContainer}>
+            {GRID_IMAGES.map((src, index) => (
+            <View key={index} style={styles.gridCell}>
+                <Image source={src} style={styles.gridImage} resizeMode="cover" />
+            </View>
+            ))}
+        </View>
+
+        <View style={[styles.center, {height: '7%', backgroundColor: 'rgb(32, 233, 173)'}]}>
+            <TouchableOpacity onPress={() => Alert.alert('Rupurudu!')}>
+                <Text>Click me!!</Text>
+            </TouchableOpacity>
+        </View>
 
     </View>
     // <View>
@@ -78,13 +101,35 @@ const styles = StyleSheet.create({
         // width: 200,
         // height: 100,
         paddingVertical: 20,
-        paddingHorizontal: '75%',
+        paddingHorizontal: 65,
         backgroundColor: 'rgb(118, 162, 255)',
     },
 
     buttonRow2: {
         backgroundColor: 'rgb(237, 154, 136)'
-    }
+    },
+
+    gridContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        margin: '3.5%',
+        justifyContent: 'center',
+    },
+
+    gridCell: {
+        width: '30%',
+        aspectRatio: 1,
+        marginHorizontal: '1.5%',   
+        marginBottom: '3%',
+        borderRadius: 4,
+        overflow: 'hidden',
+        // backgroundColor: '#FFA500',
+    },
+
+    gridImage: {
+        width: '100%',
+        height: '100%',
+    },
 })
 
 export default index
